2db9892ceb2307620de3fb0d44396dbea9bd5c45
Art Furrer <2823246+bonham@users.noreply.github.com>
2025-08-04 21:27:41 +0200

    fix bootstrap

--- js/src/main.ts
+++ js/src/main.ts
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import '@/../scss/styles.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
