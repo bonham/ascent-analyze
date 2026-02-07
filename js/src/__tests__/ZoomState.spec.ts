db1b3bedaa30cdb8c9af850cc19f20ba6e30c5f1
Art Furrer <2823246+bonham@users.noreply.github.com>
2026-02-07 17:57:57 +0100

    strip test

--- js/src/__tests__/ZoomState.spec.ts
+++ js/src/__tests__/ZoomState.spec.ts
@@ -1,47 +1,12 @@
-import { ZoomState } from "@/lib/elevationChart/ZoomState";
+import { ZoomPanState } from "@/lib/elevationChart/ZoomState";
 import { beforeEach, describe, test, expect } from "vitest";
 
 describe("ZoomState", () => {
-  let zs: ZoomState
+  let zs: ZoomPanState
   beforeEach(() => {
-    zs = new ZoomState(Math.log(2), { start: 0, end: 10 })
+    zs = new ZoomPanState(Math.log(2), { start: 0, end: 10 })
   })
   test("Initial state", () => {
-    expect(zs.accumulatedDelta()).toEqual(0)
-    expect(zs.zoomInProgress()).toBeFalsy()
-    expect(zs.xPositionForZoom()).toBeUndefined()
+    expect(zs.hasChanged()).toBeFalsy()
   })
-
-  test("Startstop", () => {
-    // stopped
-    zs.zoomTransformation(-2, 13)
-    expect(zs.xPositionForZoom()).toBeUndefined()
-
-    zs.zoomTransformation(-1, 5)
-
-    // start zoom, this should return current delta and reset to 0
-    const streched = zs.startZoom() // should reset delta
-    expect(streched.start).toEqual(2)
-    expect(streched.end).toEqual(8)
-    expect(zs.accumulatedDelta()).toEqual(0)
-    expect(zs.zoomInProgress()).toBeTruthy()
-    expect(zs.xPositionForZoom()).toEqual(5) // last buffer transformation before start
-
-    // continue adding delta while status is in progress
-    zs.zoomTransformation(1, 15)
-    expect(zs.xPositionForZoom()).toEqual(5) // last buffer transformation before start
-    zs.zoomTransformation(5, 16)
-    expect(zs.accumulatedDelta()).toEqual(6)
-
-    // stop zoom and add more delta
-    const r = zs.zoomFinished()
-    expect(r).toBeUndefined()
-    expect(zs.accumulatedDelta()).toEqual(6)
-
-    // add more delta while zoom is stopped
-    zs.zoomTransformation(-0.5, 17)
-    expect(zs.accumulatedDelta()).toEqual(5.5)
-
-  })
-
 })
